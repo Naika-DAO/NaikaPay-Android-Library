83ddebcf61d7cc1c1a47e26f8430b9baf7a551de
Babak <babak@say.company>
2022-08-25 17:04:26 +0430

    delete unused files and libraries

--- naikapay/src/main/java/io/naika/naikapay/ABI.kt
+++
@@ -1,9 +0,0 @@
-package io.naika.naikapay
-
-data class ABI(
-    val inputs: List<Any>,
-    val stateMutability: String,
-    val type: String,
-    val name: String,
-    val outputs: List<Any>
-)
